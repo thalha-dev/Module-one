6059d086aaba4099a6eb4f8f411e93d6a458364f
thalha-dev <dev.thalha@gmail.com>
2022-07-22 17:44:57 +0530

    2d array 2 porblems

---
+++ module1/others/TwoDimensionalArray/PrintSumRows.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class PrintSumRows {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < m; j++) {
+                sum += arr[i][j];
+            }
+            System.out.println(sum);
+        }
+
+    }
+}
